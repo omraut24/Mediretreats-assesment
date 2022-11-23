0b4a23f54b5ae27ad9966264698697de3bd9be3b
Omkar Raut <omkarraut2412@gmail.com>
2022-11-23 13:48:45 +0530

    made changes in code

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
     $("#countries").msDropdown();
 })
+
+const accordion = document.getElementsByClassName('container');
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener('click', function() {
+        this.classList.toggle('active')
+    })
+}
